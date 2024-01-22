e4d5fa04b5938b0fc13d70f704390544cdb43b80
Md Mahfuz Rahman Nirob <105499709+mdmahfuz307@users.noreply.github.com>
2024-01-22 20:13:23 +0600

    Add files via upload

---
+++ 04_Javascript/03.js
@@ -0,0 +1,29 @@
+// rules for naming variables
+
+// you cannot start with number
+// example :-
+// 1value(invalid)
+// value1 (valid)
+
+var value1 = 10;
+console.log(value1);
+
+// you can only undersore - or dollar symbol
+// first_name (valid)
+// _firstname(valid)
+
+var _name = "Nirob";
+console.log(_name);
+
+// first$name (valid)
+// $firstname (valid)
+
+var first$name = "Shanto";
+console.log(first$name);
+
+// you cannot use spaces
+
+// first name (invalid)
+
+//convention
+// start with small letter and use camelCase
---
+++ 04_Javascript/04.js
@@ -0,0 +1,7 @@
+// let keyword
+//declare variable with let keyword
+
+let firstName = "Nirob";
+console.log(firstName);
+
+// block scope vs funtion scope 
